271ed39671b294f7933c9082f3a78e18350dd6da
Norbik1004 <71138014+Norbik1004@users.noreply.github.com>
2020-12-22 19:27:13 +0100

    Update dataManager.java

--- dataManager.java
+++ dataManager.java
@@ -4,7 +4,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import me.JayMar921.amongus.Main;
+import me.Norbik1004.amongus.Main;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
